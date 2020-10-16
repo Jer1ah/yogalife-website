c22ceca40226c7b3d01abbfb9d6fa1d85ebb3878
Jeriah Bowers <jeriahbowers@Jeriahs-MacBook-Air.local>
2020-10-16 13:56:05 -0400

    working on dropdown navigation

--- js/index.js
+++ js/index.js
@@ -28,9 +28,39 @@ const dropdownMenuController = (function() {
     const _dropdownMenu = document.querySelector(".dropdown-menu");
     const _menuButton = document.querySelector(".mobile-nav img");
     const _backdrop = document.querySelector(".backdrop");
+    const _closeIcon = document.querySelector(".close-icon");
+    const _navigation = document.querySelector(".navigation__list");
 
     _menuButton.addEventListener("click", () => {
-        _dropdownMenu.style.transform = "scale(450)";
+        _dropdownMenu.style.transform = "scale(430)";
         _backdrop.style.display = "inline-block";
+        _closeIcon.style.display = "inline-block";
+        _navigation.style.display = "inline-block";
     });
+
+    _closeIcon.addEventListener("click", () => {
+        _dropdownMenu.style.transform = "scale(1)";
+        _closeIcon.style.display = "none";
+        _backdrop.style.display = "none";
+        _navigation.style.display = "none";
+    });
+}());
+
+const windowResizeController = (function() {
+    const _navigation = document.querySelector(".navigation__list");
+    const _dropdownMenu = document.querySelector(".dropdown-menu");
+    const _backdrop = document.querySelector(".backdrop");
+    const _closeIcon = document.querySelector(".close-icon");
+
+    window.onresize = () => {
+        if( window.innerWidth > 925 ) {
+            _navigation.style.display = "flex";
+        } else {
+            _navigation.style.display = "none";
+        }
+
+        _dropdownMenu.style.transform = "scale(1)";
+        _closeIcon.style.display = "none";
+        _backdrop.style.display = "none";
+    };
 }());
